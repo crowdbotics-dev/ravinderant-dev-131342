9449b3cf04f3744748d325a98f53397751b1f1a2
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-12-07 11:06:40 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "ravinderant-dev-131342",
-  url: "https://ravinderant-dev-131342.botics.co",
-  api: "https://ravinderant-dev-131342.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "ravinderant-dev-131342",
+  "url": "https://ravinderant-dev-131342.botics.co",
+  "api": "https://ravinderant-dev-131342.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
